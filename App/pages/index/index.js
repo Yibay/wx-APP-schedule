8d8508ceda4e6b67fe70b6a91e1fe991ab1f5690
Yibay <514531536@qq.com>
2017-12-30 15:25:02 +0800

    日历模块 切换月份功能

--- App/pages/index/index.js
+++ App/pages/index/index.js
@@ -4,10 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    date: {}, // 日历组件：日期数据
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,40 +12,105 @@ Page({
       url: '../logs/logs'
     })
   },
+  // 生命周期：页面加载
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    // 获取当前选中日期
+    var date = this.data.date.datetime || new Date()
+    // 初始化日历组件
+    this.initCalendar(date)
+  },
+
+  /** 
+   * 组件方法：初始化日历组件
+   * @param {Date} date
+   */
+  initCalendar: function(date){
+    // 构造日历数据
+    var dateData = this.constructCalendarData(date)
+    // 更新日历数据
+    this.setCalendarData(dateData)
+  },
+  /** 
+   * 组件方法：初始日历数据
+   * @param {Date} date
+   * @returns {Object} calendarData
+   */
+  constructCalendarData: function(date){
+    var currentYear = date.getFullYear() // 年
+    var currentMonth = date.getMonth() // 月
+    var currentDate = date.getDate() // 日
+    // 月初星期几（0~6 星期日~星期六）
+    var startDay = new Date(currentYear, currentMonth, 1).getDay()
+    // 本月天数
+    var numOfDay = new Date(currentYear, currentMonth + 1, 0).getDate()
+    // 本月天数组
+    var days = new Array(numOfDay)
+    for (var i = 0; i < numOfDay; i++) {
+      days[i] = { id: i + 1 }
+    }
+    return {
+      datetime: date,
+      currentYear,
+      currentMonth,
+      currentDate,
+      startDay,
+      numOfDay,
+      days
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  /** 
+   * 组件方法：更新日历数据 刷新视图
+   * @param {Object} calendarData
+   */
+  setCalendarData(calendarData){
+    console.log(calendarData)
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      'date.datetime': calendarData.datetime,
+      'date.currentYear': calendarData.currentYear,
+      'date.currentMonth': calendarData.currentMonth,
+      'date.currentDate': calendarData.currentDate,
+      'date.startDay': calendarData.startDay,
+      'date.days': calendarData.days
     })
+  },
+
+  /* 组件事件：去上一月 */
+  handleToLastMonth: function () {
+    // 月份减1；跨年 则年减1，月设为 11 (12月份)
+    var currentMonth, currentYear = this.data.date.currentYear;
+    if (this.data.date.currentMonth > 0) { // 0 为1月份
+      currentMonth = this.data.date.currentMonth - 1
+    }
+    else {
+      currentYear --;
+      currentMonth = 11 // 11 为12月份
+    }
+    // 构造日期数据（上月1号, 不用当前日，防止超出上月最大天数）
+    var dateData = this.constructCalendarData(new Date(currentYear, currentMonth, 1))
+    // 调整 日值 (若超出上月天数，则取上月最大值)
+    dateData.currentDate = Math.min(dateData.numOfDay, this.data.date.currentDate)
+    dateData.datetime = new Date(dateData.currentYear, dateData.currentMonth, dateData.currentDate)
+    // 更新日历数据
+    this.setCalendarData(dateData)
+  },
+
+  /* 组件事件：去下一月 */
+  handleToNextMonth: function () {
+    // 月份加1；跨年 则年加1，月设为 0 (1月份)
+    var currentMonth, currentYear = this.data.date.currentYear;
+    if (this.data.date.currentMonth < 11) { // 11 为12月份
+      currentMonth = this.data.date.currentMonth + 1
+    }
+    else {
+      currentYear++;
+      currentMonth = 0 // 0 为1月份
+    }
+    // 构造日期数据（下月1号）
+    var dateData = this.constructCalendarData(new Date(this.data.date.currentYear, this.data.date.currentMonth + 1, 1))
+    // 调整 日值
+    dateData.currentDate = Math.min(dateData.numOfDay, this.data.date.currentDate)
+    dateData.datetime = new Date(dateData.currentYear, dateData.currentMonth, dateData.currentDate)
+    // 更新日历数据
+    this.setCalendarData(dateData)
   }
 })
--- App/utils/util.js
+++ App/utils/util.js
@@ -1,3 +1,4 @@
+// 格式化时间 字符串 如：2017/12/29 17:51:35
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -9,11 +10,13 @@ const formatTime = date => {
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
+// 格式化数字 不足2位，补一个0
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
 
+// 暴露接口
 module.exports = {
   formatTime: formatTime
 }
