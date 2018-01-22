b4b3fed135bba2da4599dee792f77d73a88c008e
Yibay <514531536@qq.com>
2018-01-22 22:36:56 +0800

    1，新建行程 活动主题、活动时间选择、备注信息；

--- App/pages/addActivity/addActivity.js
+++ App/pages/addActivity/addActivity.js
@@ -3,8 +3,29 @@
 const app = getApp();
 
 Page({
+  data: {
+    activityStartTime: '00:00',
+    activityEndTime: '00:00'
+  },
   // 生命周期：页面加载
   onLoad(){
     console.log(app.globalData.current);
+  },
+
+  // 取消活动
+  cancel(){
+    wx.navigateBack({
+      delta: 1, // 返回上一页
+    })
+  },
+
+  // 活动起始时间变更
+  startTimeChange(e){
+    this.setData({ activityStartTime: e.detail.value });
+  },
+
+  // 活动结束时间变更
+  endTimeChange(e){
+    this.setData({ activityEndTime: e.detail.value });
   }
 })
