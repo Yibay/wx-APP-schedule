9092c4766b3ddc380193a169fa6c056d75fdb1c4
Yibay <514531536@qq.com>
2018-01-28 22:25:37 +0800

    1，日历组件，进一步抽象把记录数据源、切换日期回调 抽象出来

--- App/components/calendar/calendar.js
+++ App/components/calendar/calendar.js
@@ -1,38 +1,60 @@
 const _ = require('../../utils/util.js')
 
-// 日历组件
+/** 
+ * <calendar bind:getDate = 'getDate' calendar-records='{{calendarRecords}}' />
+ * 可绑回调事件 getDate, 选中日期，用于下方显示 当日内容
+ * 可传入记事对象 calendarRecords，包含月记录内容（3个月），用于渲染 wxml
+ */
+
+/*日历组件*/
 Component({
-  // 组件内数据
-  data: {
-    date: {}, // 日历组件：日期数据
-    swiperIndex: 1, // 3页日历 轮播，默认中间页（0、1、2）
-    duration: 300  // 轮播缓动时间
+  // 1. 传入属性
+  properties: {
+    // 各日记录，用于显示组件上内容(左下角：事项类型；右下角：颜色)
+    calendarRecords: {
+      type: Object, // {年-月:{日:事件对象}
+      value: {
+        '2018-00': {
+          // 1: {text: '...', color:'#6ad4fe'},
+          // 15: { text: '...', color: '#b7b8f0'}
+        }
+      } 
+    }
   },
-  // 组件生命周期
+  // 2. 组件内数据
+  data: {
+    // 抽象成组件后，可以考虑把date去掉，直接在data上 布置变量
+    date: {
+      // datetime: new Date(),
+    }, // 日历组件：日期数据
+    swiperIndex: 1, // 3页日历 轮播，默认中间页（0、1、2）
+    duration: 300,  // 轮播缓动时间
+  },
+  // 3. 组件生命周期
   attached(){
     // 获取当前选中日期
     var date = this.data.date.datetime || new Date()
     // 初始化日历组件
     this.updateCalendar(date)
   },
-  // 组件方法
+  // 4. 组件方法
   methods: {
     /** 
-     * 组件方法：初始化/更新 日历组件
+     * 4.1 组件方法：初始化/更新 日历组件
      * @param {Date} date
      */
-    updateCalendar: function (date) {
+    updateCalendar(date) {
       // 构造日历数据
       var dateData = this.constructCalendarData(date)
       // 更新日历数据
       this.setCalendarData(dateData)
     },
     /** 
-     * 构造data数据方法：构造 日历完整数据
+     * 4.1.1 构造data数据方法：构造 日历完整数据
      * @param {Date} date
      * @returns {Object} calendarData
      */
-    constructCalendarData: function (date) {
+    constructCalendarData(date) {
       var currentYear = date.getFullYear() // 年
       var currentMonth = date.getMonth() // 月
       var currentDate = date.getDate() // 日
@@ -55,7 +77,7 @@ Component({
       }
     },
     /** 
-     * 构造data数据方法：构造 日历月swiper数据
+     * 4.1.2 构造data数据方法：构造 日历月swiper数据
      * @param {Date} date
      * @returns {{
      *  beforeDays: array,
@@ -78,28 +100,24 @@ Component({
       for (var i = 0; i < 6 - endDay; i++) {
         afterDays[i] = { id: `after${i}` };
       }
-      // 获取本地储存 记录对象 (当前月)
-      var record_key = `${currentYear}-${_.formatNumber(currentMonth)}`
-      var record = wx.getStorageSync(record_key) || {}
-      console.log(record)
+      // 记录对象 (当前月key) 配合 days内的id，可以定位到properties calendarRecords中的各日record
+      var month_record_key = `${currentYear}-${_.formatNumber(currentMonth)}`;
       // 本月天数
       var numOfDay = new Date(currentYear, currentMonth + 1, 0).getDate()
       // 本月天数组
       var days = new Array(numOfDay)
       for (var i = 0; i < numOfDay; i++) {
-        days[i] = {
-          id: i + 1,
-          record: record[_.formatNumber(i + 1)]
-        }
+        days[i] = { id: i + 1 };
       }
       return {
+        month_record_key,
         beforeDays, // 用于wxml中填补前置空格 arrayOf( { id: before0 } )
-        afterDays, // 用于wxml中填补后续空格 arrayOf( { id: after0 } )
-        days // 本月天数数组，用于 wxml 展示 arrayOf( { id: 1, record: object } )
+        days, // 本月天数数组，用于 wxml 展示 arrayOf( { id: 1, record: object } )
+        afterDays // 用于wxml中填补后续空格 arrayOf( { id: after0 } )
       }
     },
     /** 
-     * 组件方法：更新日历数据 刷新视图
+     * 4.1.3 组件方法：更新日历数据 刷新视图
      * @param {Object} calendarData
      */
     setCalendarData(calendarData) {
@@ -111,62 +129,41 @@ Component({
         'date.calendarSwiperArray': calendarData.calendarSwiperArray,
         swiperIndex: 1
       })
+      // 更新数据时，可以把某些结果，通过 triggerEvent回调 回传
+      this.triggerEvent('getDate', { datetime: calendarData.datetime });
     },
 
-    /* 组件事件：去上一月 */
-    handleToLastMonth: function () {
-      // 月份减1；跨年 则年减1，月设为 11 (12月份)
-      var currentMonth, currentYear = this.data.date.currentYear;
-      if (this.data.date.currentMonth > 0) { // 0 为1月份
-        currentMonth = this.data.date.currentMonth - 1
-      }
-      else {
-        currentYear--;
-        currentMonth = 11 // 11 为12月份
-      }
-      // 构造日期数据（上月1号, 不用当前日，防止超出上月最大天数）
-      var dateData = this.constructCalendarData(new Date(currentYear, currentMonth, 1))
-      // 调整 日值 (若超出上月天数，则取上月最大值)
-      dateData.currentDate = Math.min(dateData.numOfDay, this.data.date.currentDate)
-      dateData.datetime = new Date(dateData.currentYear, dateData.currentMonth, dateData.currentDate)
+    /* 4.2.1 日历组件交互事件：去上一月 */
+    handleToLastMonth() {
+      // 取上月最后1天，防止 日超出当月最大天数、跨月
+      let new_date = new Date(this.data.date.currentYear, this.data.date.currentMonth, 0);
+      let currentMonth = new_date.getMonth();
+      let currentYear = new_date.getFullYear();
+      // (若超出上月天数，则取上月最大值)
+      let currentDate = Math.min(new_date.getDate(), this.data.date.currentDate);
+
+      // 构造日期数据
+      var dateData = this.constructCalendarData(new Date(currentYear, currentMonth, currentDate));
       // 更新日历数据
-      this.setCalendarData(dateData)
+      this.setCalendarData(dateData);
     },
 
-    /* 组件事件：去下一月 */
-    handleToNextMonth: function () {
-      // 月份加1；跨年 则年加1，月设为 0 (1月份)
-      var currentMonth, currentYear = this.data.date.currentYear;
-      if (this.data.date.currentMonth < 11) { // 11 为12月份
-        currentMonth = this.data.date.currentMonth + 1
-      }
-      else {
-        currentYear++;
-        currentMonth = 0 // 0 为1月份
-      }
-      // 构造日期数据（下月1号）
-      var dateData = this.constructCalendarData(new Date(this.data.date.currentYear, this.data.date.currentMonth + 1, 1))
-      // 调整 日值
-      dateData.currentDate = Math.min(dateData.numOfDay, this.data.date.currentDate)
-      dateData.datetime = new Date(dateData.currentYear, dateData.currentMonth, dateData.currentDate)
+    /* 4.2.2 日历组件交互事件：去下一月 */
+    handleToNextMonth() {
+      // 取下月最后1天，防止 日超出当月最大天数、跨月
+      let new_date = new Date(this.data.date.currentYear, this.data.date.currentMonth + 2, 0);
+      let currentMonth = new_date.getMonth();
+      let currentYear = new_date.getFullYear();
+      // (若超出上月天数，则取上月最大值)
+      let currentDate = Math.min(new_date.getDate(), this.data.date.currentDate);
+
+      // 构造日期数据
+      var dateData = this.constructCalendarData(new Date(currentYear, currentMonth, currentDate));
       // 更新日历数据
-      this.setCalendarData(dateData)
+      this.setCalendarData(dateData);
     },
 
-    /* 组件事件：切换日期 */
-    handleChangeDate: function (evt) {
-      // 若没点中 日期数据，则直接返回
-      if (!evt.target.dataset.day) return;
-      // 更新日历数据
-      this.updateCalendar(new Date(this.data.date.currentYear, this.data.date.currentMonth, evt.target.dataset.day))
-    },
-
-    /* 组件事件：切换到今天 */
-    changeToToday: function () {
-      this.updateCalendar(new Date());
-    },
-
-    /* 日历组件事件：滑到上、下一月 */
+    /* 4.2.3 日历组件交互事件：滑到上、下一月 */
     swiperCalendar(evt) {
       if (evt.detail.current === 2) {
         new Promise((resolve) => {
@@ -174,7 +171,7 @@ Component({
             this.setData({ duration: 0 });
             this.handleToNextMonth();
             resolve();
-          }, 200)
+          }, 300)
         })
           .then(
           () => { this.setData({ duration: 300 }); }
@@ -186,12 +183,30 @@ Component({
             this.setData({ duration: 0 });
             this.handleToLastMonth();
             resolve();
-          }, 200)
+          }, 250)
         })
           .then(
           () => { this.setData({ duration: 300 }); }
           )
       }
+    },
+
+    /* 4.3 日历组件交互事件：切换日期 */
+    handleChangeDate(evt) {
+      // 若没点中 日期数据，则直接返回
+      if (!evt.target.dataset.day) return;
+      // 更新日历数据
+      this.updateCalendar(new Date(this.data.date.currentYear, this.data.date.currentMonth, evt.target.dataset.day))
+    },
+
+    /* 4.3.1 日历组件交互事件：切换到今天 */
+    changeToToday() {
+      this.updateCalendar(new Date());
+    },
+
+    /* 4.4 添加当日事件 */ 
+    addActivity(){
+      this.triggerEvent('addActivity');
     }
   }
 })
--- App/pages/index/index.js
+++ App/pages/index/index.js
@@ -1,22 +1,74 @@
 //index.js
-//获取应用实例
-const app = getApp()
-// 引入工具函数
-const _ = require('../../utils/util.js')
+const _ = require('../../utils/util.js');
+const app = getApp();
 
 Page({
   data: {
+    // datetime: Date 类型
+    record_key: '', // 如 '2017-01'
+    calendarRecords: {} // 如 {'2018-01':{'04':...,'26':...}}，可以初始化时，传入全部数据，本项目时，翻页时，3月1批更新；
   },
   // 生命周期：页面显示
   onShow: function () {
+    this.updateRecord(this.data.datetime);
   },
 
-  /* 组件事件：添加活动 */
+  /* 1. 传入calendar组件的回调函数，获取 calendar组件 当前日期 */
+  getDate(evt) {
+    // 刷新日历记录
+    this.checkUpdateRecord(evt.detail.datetime);
+    // 把calendar传出的数据，绑定到data上
+    this.setData({ datetime: evt.detail.datetime });
+  },
+  /* 1.1 检验是否需要更新 */
+  checkUpdateRecord(datetime){
+    let currentDate = new Date(datetime);
+    let currentYear = currentDate.getFullYear() // 年
+    let currentMonth = currentDate.getMonth() // 月
+    let record_key = `${currentYear}-${_.formatNumber(currentMonth)}`;
+    // 若发生过 翻月
+    if (record_key !== this.data.record_key) {
+      this.updateRecord(datetime);
+    }
+  },
+  /* 1.2 更新data数据：获取本地储存，刷新日历记录 */
+  updateRecord(datetime){
+    let currentDate = new Date(datetime);
+    let currentYear = currentDate.getFullYear() // 年
+    let currentMonth = currentDate.getMonth() // 月
+    let record_key = `${currentYear}-${_.formatNumber(currentMonth)}`;
+    // 获取本地储存（本月）
+    let calendarRecords = {};
+    // 获取本地储存（上月、本月、下月）
+    this.getAdjacentMonths(currentYear, currentMonth).forEach(record_key => {
+      let monthRecord = wx.getStorageSync(record_key) || {};
+      calendarRecords[record_key] = monthRecord;
+    });
+    console.log(calendarRecords);
+    // 一批更新3页数据，轮播时，体验好（目标页进入时，就带有数据）
+    this.setData({
+      record_key,
+      calendarRecords
+    })
+  },
+  /* 1.3 辅助工具方法：计算上月下月 record_key 如：['2017-12'，'2018-01'，'2018-02'] */
+  getAdjacentMonths(currentYear, currentMonth){
+    let adjacentMonths = [];
+    let lastMonth = new Date(currentYear, currentMonth - 1, 1);
+    let nextMonth = new Date(currentYear, currentMonth + 1, 1);
+    adjacentMonths.push(`${lastMonth.getFullYear()}-${_.formatNumber(lastMonth.getMonth())}`);
+    adjacentMonths.push(`${currentYear}-${_.formatNumber(currentMonth)}`);
+    adjacentMonths.push(`${nextMonth.getFullYear()}-${_.formatNumber(nextMonth.getMonth())}`);
+    return adjacentMonths;
+  },
+
+  /* 2.1 组件事件：添加活动 */
   addActivity: function(){
     // 当日年、月、日 数据同步到全局数据 (如此 新页面也可获得日期数据)
-    app.globalData.current.currentYear = this.data.date.currentYear;
-    app.globalData.current.currentMonth = this.data.date.currentMonth;
-    app.globalData.current.currentDate = this.data.date.currentDate;
+    let date = new Date(this.data.datetime);
+    app.globalData.current.currentYear = date.getFullYear();
+    app.globalData.current.currentMonth = date.getMonth();
+    app.globalData.current.currentDate = date.getDate();
     // 新建行程页
     wx.navigateTo({
       url: '../addActivity/addActivity',
